c6696a613e6bab3d0e969cd752778f6a2b14d141
Caitlin Wang <caitlinwang97@gmail.com>
2018-11-03 18:20:24 -0400

    got text to show up on circle and not be huge

--- SwitchNTell/Objects/GameSpace.swift
+++ SwitchNTell/Objects/GameSpace.swift
@@ -27,40 +27,68 @@ class GameSpace: SCNNode {
         self.sphere.materials = [material]
         
         self.sphereNode = SCNNode(geometry: sphere)
-        self.sphereNode.position = SCNVector3(0,0,-0.5)
+        self.sphereNode.position = SCNVector3(x: 0, y: 0, z: -0.5)
         
-        let boundingBoxMin = sphere.boundingBox.min
-        let boundingBoxMax = sphere.boundingBox.max
+//        let boundingBoxMin = sphere.boundingBox.min
+//        let boundingBoxMax = sphere.boundingBox.max
+
+//        let boundingRect = CGRect(x: CGFloat(sphereNode.position.x),
+//                                  y: CGFloat(sphereNode.position.y),
+//                                  width: sphere.radius * 2,
+//                                  height: sphere.radius * 2)
+//        let boundingRect = CGRect(origin: .zero, size: CGSize(width: 100.0, height: 500.0))
+    
         
         // Question
         self.question = SCNText(string: "What is your most traumatic memory?", extrusionDepth: 0.0)
-        self.question.font = UIFont (name: "Arial", size: 1)
-        self.question.isWrapped = true;
+//        self.question.containerFrame = boundingRect
+        self.question.font = UIFont (name: "Arial", size: 2)
+//        self.question.isWrapped = true;
+        
         
         self.questionNode = SCNNode(geometry: question)
-        self.questionNode.position = SCNVector3(0, 0, 0)
-        self.questionNode.scale = SCNVector3(0.1, 0.1, 0.1)
+        self.questionNode.position = SCNVector3(x: sphereNode.position.x,
+                                                y: sphereNode.position.y,
+                                                z: sphereNode.position.z + Float(sphere.radius))
         
+        let (min, max) = questionNode.boundingBox
+        
+        let textWidth = max.x - min.x
+        let textHeight = max.y - min.y
+        let sphereWidth = sphere.radius * 2;
+        let sphereHeight = sphere.radius * 2;
+        let scale = sphereWidth / CGFloat(textWidth);
+        
+        print(scale)
+        self.questionNode.scale = SCNVector3(0.01, 0.01, 0.01)
+    
+        
+        let dx = min.x + 0.5 * (max.x - min.x)
+        let dy = min.y + 0.5 * (max.y - min.y)
+        let dz = min.z + 0.5 * (max.z - min.z)
+        self.questionNode.pivot = SCNMatrix4MakeTranslation(dx, dy, dz)
+        
+        //2. Get The Width & Height Of Our Node
         super.init()
         
-        self.center(node: self.questionNode)
+//        self.center(node: self.questionNode)
         
-        self.addChildNode(sphereNode)
         self.addChildNode(questionNode)
+        self.addChildNode(sphereNode)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func center(node: SCNNode) {
-        let (min, max) = node.boundingBox
-        
-        let dx = min.x + 0.5 * (max.x - min.x)
-        let dy = min.y + 0.5 * (max.y - min.y)
-        let dz = min.z + 0.5 * (max.z - min.z)
-        node.pivot = SCNMatrix4MakeTranslation(dx, dy, dz)
-    }
+//    func center(node: SCNNode) {
+//        let (min, max) = node.boundingBox
+//
+//        let dx = min.x + 0.5 * (max.x - min.x)
+//        let dy = min.y + 0.5 * (max.y - min.y)
+//        let dz = min.z + 0.5 * (max.z - min.z)
+//        node.pivot = SCNMatrix4MakeTranslation(dx, dy, dz)
+//    }
 }
 
 
