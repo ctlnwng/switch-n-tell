5eafda389390b4f3990fc23b2fe569be0abc243e
Noah Appleby <appleby.n@husky.neu.edu>
2018-11-03 13:50:29 -0400

    add vr tap balls demo

--- SwitchNTell/ViewController.swift
+++ SwitchNTell/ViewController.swift
@@ -26,7 +26,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -39,6 +41,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     private func setupNextVC() {
         self.onboarding = STOnboardingViewController()
+        
+        //Create TapGesture Recognizer
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        sceneView.addGestureRecognizer(tap)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,9 +52,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = [.horizontal]
 
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                self.addPlane(node: node, anchor: planeAnchor)
+            }
+//        }
+    }
+    
+    func addPlane(node: SCNNode, anchor: ARPlaneAnchor) {
+        let plane = Plane(anchor)
+        node.addChildNode(plane)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -92,5 +113,53 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    var nodes: [SCNNode] = []
+    
+    var nodeColor = UIColor.orange;
+    var nodeRadius = CGFloat.init(0.1);
+    
+    // MARK: Gesture handlers
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        
+        let tapLocation = sceneView.center// Get the center point, of the SceneView.
+        let hitTestResults = sceneView.hitTest(tapLocation, types:.featurePoint)
+        
+        if let result = hitTestResults.first {
+            let position = SCNVector3.positionFrom(matrix: result.worldTransform)
+            let sphere = SCNSphere(color: self.nodeColor, radius: CGFloat(self.nodeRadius))
+            let node = SCNNode(geometry: sphere)
+            node.position = position;
+            sceneView.scene.rootNode.addChildNode(node)
+            nodes.append(node)
+        }
+    }
+
+    
+    // Code Example from https://github.com/kravik/ArMeasureDemo/blob/master/ArMeasureDemo/ViewController.swift
+}
+
+extension SCNSphere {
+    convenience init(color: UIColor, radius: CGFloat) {
+        self.init(radius: radius)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = color
+        materials = [material]
+    }
+}
+
+extension SCNVector3 {
+    func distance(to destination: SCNVector3) -> CGFloat {
+    let dx = destination.x - x
+    let dy = destination.y - y
+    let dz = destination.z - z
+    return CGFloat(sqrt(dx*dx + dy*dy + dz*dz))
+    }
+    
+    static func positionFrom(matrix: matrix_float4x4) -> SCNVector3 {
+    let column = matrix.columns.3
+    return SCNVector3(column.x, column.y, column.z)
     }
 }
