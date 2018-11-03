fc8c919785f74c2ef9623177d12fca448b33d3b4
baharsheikhi <haji-sheikhi.b@husky.neu.edu>
2018-11-03 17:03:13 -0400

    add second screen for asking for # of players

---
+++ SwitchNTell/STNumPlayersOnboardingViewController.swift
@@ -0,0 +1,44 @@
+//
+//  STNumPlayersOnboardingViewController.swift
+//  SwitchNTell
+//
+//  Created by Bahar Sheikhi on 11/3/18.
+//  Copyright © 2018 SwitchNTell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class STNumPlayersOnboardingViewController : UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var numPlayersTextField: UITextField!
+    private var numPlayers: String?
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        self.numPlayersTextField.delegate = self
+        self.numPlayersTextField.becomeFirstResponder()
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (self.numPlayersTextField.isFirstResponder) {
+            self.numPlayersTextField.resignFirstResponder()
+        } else {
+            self.numPlayersTextField.becomeFirstResponder()
+        }
+        self.numPlayers = numPlayersTextField.text
+        return true
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "goToBoardSetup")
+        {
+            // send to the STSettings
+        }
+    }
+    
+    
+}
