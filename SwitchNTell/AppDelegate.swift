646c5837e5c2f4318c39eac342b50a4f9ca4d3da
Noah Appleby <appleby.n@husky.neu.edu>
2018-11-04 03:16:29 -0500

    add splash tap to continue

--- SwitchNTell/AppDelegate.swift
+++ SwitchNTell/AppDelegate.swift
@@ -17,9 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let sb = UIStoryboard(name: "Main", bundle: nil)
-        let vc = sb.instantiateViewController(withIdentifier: "STOnboarding")
+        let vc = sb.instantiateViewController(withIdentifier: "SplashScreen")
         let navC = UINavigationController.init(rootViewController: vc)
         UINavigationBar.appearance().tintColor = UIColor.init(red: 236/255, green: 191/255, blue: 0, alpha: 1)
+        navC.setNavigationBarHidden(true, animated: false)
         //self.present(navC, animated: true, completion: nil)
         self.window?.rootViewController = navC
         return true
